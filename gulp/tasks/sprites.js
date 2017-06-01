ea17ce66f11dd9b008d17a4056b98d684662e62f
Naill <2jetsetter@gmail.com>
2017-06-01 23:42:48 +0300

    sprite

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp'),
+    svgSprite = require('gulp-svg-sprite');
+
+const config = {
+   mode: {
+      css: {
+         render: {
+            css: {
+               template: './gulp/templates/sprite.css'
+            }
+         }
+
+      }
+   }
+}
+
+gulp.task('createSprite', function () {
+   return gulp.src('./app/assets/images/icons/**/*.svg')
+       .pipe(svgSprite(config))
+       .pipe(gulp.dest('./app/temp/sprite/'))
+});
